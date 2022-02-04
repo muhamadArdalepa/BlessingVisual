bde21f0bda09f38ee35713a5ad5dc63cb4569459
muhamadArdalepa <69201192+muhamadArdalepa@users.noreply.github.com>
2022-02-04 10:06:20 +0700

    Add files via upload

---
+++ src/blessingvisual/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blessingvisual;
+
+/**
+ *
+ * @author Kamisato Ayaka
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        model.updOrIns("delete from tbl_jenis where id_jenis=1");
+    }
+}
